82ab94f09d1c68729db3ea804f6d64b69cc73699
karkuak <43709640+karkuak@users.noreply.github.com>
2019-11-14 22:11:10 -0800

    revert changes

--- testfile.java
+++ testfile.java
@@ -9,10 +9,6 @@ public class testfile
     public static void main( String[] args )
     {
         System.out.println( "Hello ducks!" );
-        System.out.println( addition(7,9));
-    }
-    
-    public static int addition(int a, int b) {
-    	return a+b;
+        System.out.println( "Hello all the ducks!" );
     }
 }
