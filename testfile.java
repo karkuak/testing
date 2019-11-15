2d4ac4d1444126b78896629331cf0f194394ec77
karthik <kaarthikk.sivakumar@gmail.com>
2019-11-14 21:50:27 -0800

    Adding a new file

---
+++ testfile.java
@@ -0,0 +1,13 @@
+package testngpractice.algos2;
+
+/**
+ * Hello world!
+ *
+ */
+public class testfile 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello ducks!" );
+    }
+}
