66a9dae008a28c5eb796cbbad7fef40c78425fdc
karthik <kaarthikk.sivakumar@gmail.com>
2019-11-14 22:08:15 -0800

    Implemented addition

--- testfile.java
+++ testfile.java
@@ -9,6 +9,10 @@ public class testfile
     public static void main( String[] args )
     {
         System.out.println( "Hello ducks!" );
-        System.out.println( "Hello all the ducks!" );
+        System.out.println( addition(7,9));
+    }
+    
+    public static int addition(int a, int b) {
+    	return a+b;
     }
 }
